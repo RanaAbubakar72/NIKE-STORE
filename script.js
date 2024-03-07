26d472699bc651e55296d2ca4c55f063848a9b78
RanaAbubakar72 <abubakarrana982@gmail.com>
2024-03-07 18:02:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const wrapper = document.querySelector(".sliderWrapper")
+const menuItem = document.querySelectorAll(".menuItem")
+console.log(menuItem);
+
+menuItem.forEach((item, index)=>{
+    item.addEventListener("click", ()=>{
+      wrapper.style.transform = `translateX(${-100 * index}vw)`
+    })
+})
+
+// wrapper.style.backgroundColor="red"
